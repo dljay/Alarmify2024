c47c707c84b809ce9f75f11f790bed216efea18b
JJONGMYAN <lune856@naver.com>
2021-08-12 00:37:51 +0900

    // v.0.02d : FB/RcView 테스트 중- RcView 에 이미지 들어오네. - Dark Theme 여서 글씨 안 보이는 것 파악, digging - MyIaP 음.. 일단은 둘까?

---
+++ app/src/main/java/com/theglendales/alarm/jjfirebaserepo/FirebaseRepoClass.kt
@@ -0,0 +1,54 @@
+package com.theglendales.alarm.jjfirebaserepo
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+
+const val TAG ="FBRepoClass"
+class FirebaseRepoClass
+{
+    private val firebaseFSInstance : FirebaseFirestore = FirebaseFirestore.getInstance()
+    lateinit var dbCollectionReference: CollectionReference
+
+    //1) Get all data from Firebase . 2) 코루틴 사용 안하고 callback 사용 3) google.
+    fun getPostList(): Task<QuerySnapshot> { // return type: Task snapshot!
+        dbCollectionReference = firebaseFSInstance.collection("ringtones")
+        return dbCollectionReference.orderBy("id", Query.Direction.ASCENDING).get()
+
+    }
+
+    fun sortSingleOrMultipleTags(tagsList: MutableList<String>): Task<QuerySnapshot> {
+        // 약간 멍청해보이지만 this is the only way..?
+        when (tagsList.size) {
+            1 -> { // 1개 chip 선택
+                return dbCollectionReference.whereEqualTo(tagsList[0], true).get() // 이거 그냥 whereArrayContains (1개 조건 only..)로 바꿔도 됨.//containsAny = 10개 항목..
+            }
+            2 -> { // 2개 chip 선택
+                return dbCollectionReference.whereEqualTo(tagsList[0], true).whereEqualTo(tagsList[1], true).get()
+            }
+            3 -> { // 3개 chip 선택
+                return dbCollectionReference.whereEqualTo(tagsList[0], true).whereEqualTo(tagsList[1], true).whereEqualTo(tagsList[2], true).get()
+            }
+            4 -> { // 4개 chip 선택
+                return dbCollectionReference.whereEqualTo(tagsList[0], true).whereEqualTo(tagsList[1], true)
+                    .whereEqualTo(tagsList[2], true).whereEqualTo(tagsList[3], true).get()
+            }
+            5 -> { // 5개 chip 선택
+                return dbCollectionReference.whereEqualTo(tagsList[0], true).whereEqualTo(tagsList[1], true)
+                    .whereEqualTo(tagsList[2], true).whereEqualTo(tagsList[3], true).whereEqualTo(tagsList[4], true).get()
+            }
+            6 -> { // 6개 chip 선택
+                return dbCollectionReference.whereEqualTo(tagsList[0], true).whereEqualTo(tagsList[1], true)
+                    .whereEqualTo(tagsList[2], true).whereEqualTo(tagsList[3], true).whereEqualTo(tagsList[4], true).whereEqualTo(tagsList[5], true).get()
+            }
+            else-> return dbCollectionReference.whereEqualTo(tagsList[0], true).get() // 이건 그냥 혹시몰라서 when(1) 그대로 복붙!
+        }
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/theglendales/alarm/jjongadd/SecondFragment.kt
+++ app/src/main/java/com/theglendales/alarm/jjongadd/SecondFragment.kt
@@ -17,18 +17,22 @@ import com.theglendales.alarm.jjadapters.MyNetWorkChecker
 import com.theglendales.alarm.jjadapters.MyOnItemClickListener
 import com.theglendales.alarm.jjadapters.RcViewAdapter
 import com.theglendales.alarm.jjdata.RingtoneClass
-
+import com.theglendales.alarm.jjfirebaserepo.FirebaseRepoClass
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SecondFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 private const val TAG="SecondFragment"
 class SecondFragment : androidx.fragment.app.Fragment(), MyOnItemClickListener  {
 
     var fullRtClassList: MutableList<RingtoneClass> = ArrayList()
+//    var iapInstance = MyIAPHelper(this,null, ArrayList())
+    lateinit var rcvAdapterInstance: RcViewAdapter
     private val myNetworkCheckerInstance by lazy { context?.let { MyNetWorkChecker(it) } }
+    private val firebaseRepoInstance: FirebaseRepoClass = FirebaseRepoClass()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
@@ -45,7 +49,7 @@ class SecondFragment : androidx.fragment.app.Fragment(), MyOnItemClickListener  
         val rcView = this.view?.findViewById<RecyclerView>(R.id.id_rcV_2ndFrag)
         rcView?.layoutManager = LinearLayoutManager(context)
 
-        val rcvAdapterInstance = activity?.let { RcViewAdapter(ArrayList(), this, it) } // 공갈리스트 넣어서 instance 만듬
+        rcvAdapterInstance = activity?.let { RcViewAdapter(ArrayList(), this, it) }!! // 공갈리스트 넣어서 instance 만듬
         rcView?.adapter = rcvAdapterInstance
         rcView?.setHasFixedSize(true)
         // Fake FullrtClassList
@@ -69,10 +73,45 @@ class SecondFragment : androidx.fragment.app.Fragment(), MyOnItemClickListener  
             //lottieAnimController(1)
             return //더이상 firebase 로딩이고 나발이고 진행 안함!!
         }
-        else if(isInternetAvailable) {
-            Log.d(TAG, "loadFromFireBase: jj- isInternetAvailable = $isInternetAvailable")
-        }
+        else {Log.d(TAG, "loadFromFireBase: jj- isInternetAvailable = $isInternetAvailable") }
+
+
     //2. If we have internet connectivity, then call FireStore!
+        firebaseRepoInstance.getPostList().addOnCompleteListener {
+            if(it.isSuccessful)
+            {
+                Log.d(TAG, "<<<<<<<<<loadPostData: successful")
+
+                // 만약 기존에 선택해놓은 row 가 있으면 그쪽으로 이동.
+//                mySmoothScroll()
+
+                fullRtClassList = it.result!!.toObjects(RingtoneClass::class.java)
+                // IAP related: Initialize IAP and send instance <- 이게 시간이 젤 오래걸리는듯.
+
+//                iapInstance = MyIAPHelper(this, rcvAdapterInstance, fullRtClassList) //reInitialize
+//                iapInstance.refreshItemIdsAndMp3UrlMap() // !!!!!!!!!!!!!!여기서 일련의 과정을 거쳐서 rcView 화면 onBindView 까지 해줌!!
+
+
+                // Update MediaPlayer.kt
+//                mpClassInstance.createMp3UrlMap(fullRtClassList)
+
+                // Update Recycler View
+                rcvAdapterInstance.updateRecyclerView(fullRtClassList) // todo: 추후 // comment 시킬것. MyIAPHelper.kt 에서 해주기로 함!
+                rcvAdapterInstance.updateRingToneMap(fullRtClassList)// todo: 이 map 안 쓰이는것 같은데 흐음.. (우리는 Map 기반이므로 list 정보를 -> 모두 Map 으로 업데이트!)
+
+                // SwipeRefresh 멈춰 (aka 빙글빙글 animation 멈춰..)
+//                if(swipeRefreshLayout.isRefreshing) {
+//                    Log.d(TAG, "loadPostData: swipeRefresh.isRefreshing = true")
+//                    swipeRefreshLayout.isRefreshing = false
+//                }
+                // 우선 lottie Loading animation-stop!!
+//                lottieAnimController(2) //stop!
+
+            } else { // 문제는 인터넷이 없어도 이쪽으로 오지 않음. always 위에 if(it.isSuccess) 로 감.
+                Log.d(TAG, "<<<<<<<loadPostData: ERROR!! Exception message: ${it.exception!!.message}")
+//                lottieAnimController(1) // this is useless at the moment..
+            }
+        }
     }
 
 

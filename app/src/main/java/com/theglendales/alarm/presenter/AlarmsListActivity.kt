c74db42b4f1ead9923afd2e7ca80d83d7028ac0a
JJONGMYAN <lune856@naver.com>
2021-12-14 18:07:31 +0900

    //// v0.40g [Install 단계에서 설치되는 알람 설정 변경] - AlarmDatabaseHelper.java
    //- 인스톨 후 알람 defrt01.mp3 & defrt02.mp3 로 SQL 에 저장(O)
    //- 인스톨 후 생성 알람은 DetailsFrag 에서 ".rta" 붙여서 정보 보여주는것으로 해결(O) .. 얼른 유저가 RtPicker 로 Ringtone 을 바꾸길 기다릴뿐.
    //- Label 이 InstallAlarm 으로 최초  SQL 에 기입되며 -> DetailsFrag 들어가는 순간 "userCreated" 로 Label 이 변경됨! (O)
    //- Rta/Art 는 모두 내장된 파일 사용(raw, drawable) (O)

--- app/src/main/java/com/theglendales/alarm/presenter/AlarmsListActivity.kt
+++ app/src/main/java/com/theglendales/alarm/presenter/AlarmsListActivity.kt
@@ -58,15 +58,15 @@ import org.koin.core.module.Module
 import org.koin.dsl.module
 import java.util.Calendar
 
-//// v0.40c [Install 단계에서 설치되는 알람 설정 변경] - AlarmDatabaseHelper.java
+//// v0.40g [Install 단계에서 설치되는 알람 설정 변경] - AlarmDatabaseHelper.java
 
 //- 인스톨 후 알람 defrt01.mp3 & defrt02.mp3 로 SQL 에 저장(O)
+//- 인스톨 후 생성 알람은 DetailsFrag 에서 ".rta" 붙여서 정보 보여주는것으로 해결(O) .. 얼른 유저가 RtPicker 로 Ringtone 을 바꾸길 기다릴뿐.
 //- Label 이 InstallAlarm 으로 최초  SQL 에 기입되며 -> DetailsFrag 들어가는 순간 "userCreated" 로 Label 이 변경됨! (O)
 //- Rta/Art 는 모두 내장된 파일 사용(raw, drawable) (O)
-// issue: APP 설치시 최초 생성된 알람 2개 -> 파일명이 확장자가 없어서 DetailsFrag.kt > updateUi 에서 안 걸리는 문제.
 
 // 할일 ==>
-//- raw 에 있는 Defrt1 DetailsFrag 들어갔을 때 정보 안 뜸 (About the Ringtone, Info)
+
 // Gal S21 에서 왜 install 할때 인스톨이 안되는겨..
 //- fab 버튼 -> 상단 + 로 변경? => xx 후에 울립니다 시간 표시-> 상단 .. ActionBar 진화형태.
 

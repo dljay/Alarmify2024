f899092591352d5a1c4fe7eae612ca7e6a5037b9
JJONGMYAN <lune856@naver.com>
2022-04-05 14:53:12 +0900

    // 30708V1.18e29 [ListFrag- Constraint Layout 으로 바꾸기 전] 22/4/5 (화) 오후 2:52

--- app/src/main/java/com/theglendales/alarm/presenter/AlarmsListActivity.kt
+++ app/src/main/java/com/theglendales/alarm/presenter/AlarmsListActivity.kt
@@ -65,7 +65,7 @@ import org.koin.dsl.module
 import java.util.Calendar
 
 
-// 30708V1.18e28 [ListFrag- OnOffContainer LinearLayout 으로 바꾸기 전] 22/4/5 (화) 오전 11:06
+// 30708V1.18e29 [ListFrag- Constraint Layout 으로 바꾸기 전] 22/4/5 (화) 오후 2:52
 //
 // ** 하위 API (26 이하) 테스트 해볼것.
 // 1) Achievements:
